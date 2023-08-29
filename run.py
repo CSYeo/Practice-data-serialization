4faa7b1d91da45c8e574f5f08bb81260113dd5cb
CSY <creightonstanleyyeo@gmail.com>
2023-08-29 19:34:12 +0800

    Added code to parse data into a dictionary, and moved 007.txt
    Using with open we added directory containing title, name, date
    and feedback keys

---
+++ run.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import os
+#import requests
+
+directory = "{}\\data\\feedback".format(os.getcwd())
+
+posts = []
+
+for file in os.listdir(directory):
+    #print(file)
+    #Here remember to use os.listdir if not it will
+    #print directory as a string
+
+    with open(file, 'r') as data:
+        lines = file.readlines()
+#lines now will read one line at a time
+        title = lines[0].strip()
+        name = lines[1].strip()
+        date = lines[2].strip()
+        feedback = lines[3:].strip()
+
+        dict = {
+        "title": title,
+        "name": name,
+        "date": date,
+        "feedback": feedback
+        }
+
+    post.append(dict)
+
+print[post]
