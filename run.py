f5ff5a565706b3e4888b20819a2266e73a3068ff
CSY <creightonstanleyyeo@gmail.com>
2023-08-29 20:45:07 +0800

    Added the POST command to be able to upload data to http websites

--- run.py
+++ run.py
@@ -5,30 +5,38 @@ import os
 
 directory = "{}\\data\\feedback".format(os.getcwd())
 
-posts = []
+def parse(folder):
+    posts = []
 
-for file in os.listdir(directory):
-    #print(file)
-    #Here remember to use os.listdir if not it will
-    #print directory as a string
+    for file in os.listdir(folder):
+        #print(file)
+        #Here remember to use os.listdir if not it will
+        #print directory as a string
 
-    filepath = "{}\\{}".format(directory, file)
+        filepath = "{}\\{}".format(folder, file)
 
-    with open(filepath, 'r') as data:
-        lines = data.readlines()
-#lines now will read one line at a time
-        title = lines[0].strip()
-        name = lines[1].strip()
-        date = lines[2].strip()
-        feedback = ''.join(lines[3:]).strip()
+        with open(filepath, 'r') as data:
+            lines = data.readlines()
+    #lines now will read one line at a time
+            title = lines[0].strip()
+            name = lines[1].strip()
+            date = lines[2].strip()
+            feedback = ''.join(lines[3:]).strip()
 
-        dict = {
-        "title": title,
-        "name": name,
-        "date": date,
-        "feedback": feedback
-        }
+            dict = {
+            "title": title,
+            "name": name,
+            "date": date,
+            "feedback": feedback
+            }
 
-    posts.append(dict)
+        posts.append(dict)
 
-print(posts)
+    return posts
+
+to_post = parse(directory)
+
+for p in to_post:
+    response = requests.post("https://example.com/path/to/api", data=p)
+    print(response)
+    #here any action would do, we just need to call the response
